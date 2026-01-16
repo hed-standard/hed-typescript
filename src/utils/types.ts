9c76ddf0760d3775fc7357ccef1617935689f3a6
Alexander Jones <happy5214@gmail.com>
2026-01-16 14:57:33 -0600

    Port top-level parser module
    Also create utility type for values returned with separate error and
    warning Issue arrays.

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,6 +3,8 @@
  * @module utils/types
  */
 
+import type { Issue } from '../issues/issues'
+
 /**
  * A generic constructor type.
  */
@@ -14,3 +16,8 @@ export type Constructor<Type> = {
  * A generic recursive array type.
  */
 export interface RecursiveArray<Type> extends Array<Type | RecursiveArray<Type>> {}
+
+/**
+ * A value returned alongside Issue arrays representing separated errors and warnings.
+ */
+export type ReturnTupleWithErrorsAndWarnings<Type> = [Type, Issue[], Issue[]]
