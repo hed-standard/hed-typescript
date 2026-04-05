1750ca5262b1dc4abfbb84631bee0604209a3328
Alexander Jones <happy5214@gmail.com>
2026-04-04 20:36:48 -0500

    Mock import to get browser tests working again

--- browser/src/schema/loader.spec.js
+++ browser/src/schema/loader.spec.js
@@ -1,14 +1,24 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { loadSchema } from './loader'
 import { SchemaSpec } from '../../../src/schema/specs.ts'
 
+vi.mock(import('./vite-importer.js'), () => {
+  return {
+    schemaData: {
+      '../../../src/data/schemas/HED_HED8.0.0_8.0.0.xml': () => Promise.resolve(
+        '<?xml version="1.0" ?>\n' +
+        '<HED><schema></schema></HED>\n'
+      ),
+    }
+  }
+})
+
 describe('Browser Schema Loader', () => {
-  it('should return undefined when loading a bundled schema in test environment', async () => {
+  it('should return a parsed XML object when loading a dummy bundled schema in test environment', async () => {
     // Create a spec with localName to trigger bundled schema loading
     const spec = new SchemaSpec('', '8.0.0', 'HED8.0.0', '')
-    // In a test environment, this will return undefined since schemaData is empty
     const schema = await loadSchema(spec)
-    expect(schema).toBeUndefined()
+    expect(schema).toHaveProperty('HED')
   })
 
   it('should throw an error for local file loading', async () => {
